9a22c0f594eed641ff91a1ad96afbb14c4101ff6
mohammed-Alijl <moh.wa.2016@gmail.com>
2023-09-14 11:55:46 +0300

    feat: make the category repository

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\BasicRepositoryInterface;
+use App\Models\Category;
+
+class CategoryRepository implements BasicRepositoryInterface
+{
+    public function getAll()
+    {
+        return Category::get();
+    }
+
+    public function getAllForMainPage()
+    {
+        return Category::where('display_main', 1)->orderBy('display_order')->get();
+    }
+
+    public function find($id)
+    {
+        return Category::findOrFail($id);
+    }
+
+    public function create($request)
+    {
+        $category = new Category();
+        $category->name = $request->name;
+        $category->display_main = $request->display_main;
+        if ($request->display_main == 1)
+            $category->display_order = $request->display_order;
+        $category->save();
+    }
+
+    public function update($request, $id)
+    {
+        $category = $this->find($id);
+        $category->name = $request->name;
+        $category->display_main = $request->display_main;
+        if ($request->display_main == 1)
+            $category->display_order = $request->display_order;
+        else
+            $category->display_order = null;
+        $category->save();
+    }
+
+    public function delete($id)
+    {
+        $category = $this->find($id);
+        $category->delete();
+    }
+}
