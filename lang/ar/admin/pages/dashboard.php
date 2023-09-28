b0ea30d810beab02ae6533079eda0e442848b522
mohammed-Alijl <moh.wa.2016@gmail.com>
2023-09-28 23:36:09 +0300

    feat: make the index page for the dashboard

---
+++ lang/ar/admin/pages/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'dashboard' => 'لوحة التحكم',
+    'hi' => 'مرحبا بعودتك يا ',
+    'aqar.dashboard' => 'لوحة التحكم لادارة العقارات',
+    'total.aqars' => 'مجمل العقارات',
+    'aqar' => 'عقار',
+    'total.attributes' => 'مجمل المواصفات',
+    'attribute' => 'مواصفة',
+    'total.categories' => 'مجمل الأقسام',
+    'category' => 'قسم',
+    'total.zones' => 'مجمل المناطق',
+    'zone' => 'منطقة',
+];
---
+++ lang/en/admin/pages/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'dashboard' => 'Dashboard',
+    'hi' => 'Hi, welcome back',
+    'aqar.dashboard' => 'Aqar Management Dashboard.',
+    'total.aqars' => 'TOTAL AQARS',
+    'aqar' => 'Aqar',
+    'total.attributes' => 'TOTAL ATTRIBUTES',
+    'attribute' => 'Attribute',
+    'total.categories' => 'TOTAL CATEGORIES',
+    'category' => 'Category',
+    'total.zones' => 'TOTAL ZONES',
+    'zone' => 'Zone',
+];
